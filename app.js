e98d59a94069268354e472fcac5a900ab7ab7969
Kyle Devine <devinenoise@gmail.com>
2021-03-01 18:46:27 -0800

    comments

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const nav = document.querySelector('#main');
+// grab top of nav
 const topOfNav = nav.offsetTop;
 
 function fixNav() {
   if (window.scrollY >= topOfNav) {
+    // sets padding to height of navbar
     document.body.style.paddingTop = nav.offsetHeight + 'px';
+    // add the fixed nav class
     document.body.classList.add('fixed-nav');
   } else {
+    // remove padding at the top
     document.body.style.paddingTop = 0;
+    // remove the fixed-nav class
     document.body.classList.remove('fixed-nav');
   }
 }
